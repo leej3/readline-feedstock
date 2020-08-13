dc401be105a28d25964a47925939d88b9c4249a1
Isuru Fernando <isuruf@gmail.com>
2020-08-13 00:09:39 -0500

    just copy config.sub

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-autoreconf -vfi
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
 ./configure --prefix=${PREFIX}  \
             --build=${BUILD}    \
